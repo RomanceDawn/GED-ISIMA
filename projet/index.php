a967ec335276d02e21d0333eb801ae73dc3dff7a
RomanceDawn <enix70@msn.com>
2014-12-18 12:39:23 +0100

    Uplaod dans le dossier rapport

--- projet/index.php
+++ projet/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>ISIMA</title>
+        <title>ISIRapport</title>
         <script src="./javascript/dropzone.js"></script>
         <link rel="stylesheet" type="text/css" href="css/dropzone.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -13,16 +13,16 @@
     <body>
         <nav>
             <ul id="mainMenu">
-                <li><a>Ajouter</a></li>
-                <li><a>Modifier</a></li>
-                <li><a>Supprimer</a></li>
+                <li><a>Upload simple</a></li>
+                <li><a>Upload Multiple</a></li>
+                <li><a>Modifier fichier</a></li>
                 <li class="flotteDroite"><a>Déconexion</a></li>
             </ul>
         </nav>
         <div id="corp">
 
             <section id="centre">
-                <form action="/file-upload" class="dropzone" id="my-awesome-dropzone">
+                <form action="./php/multiUpload.php" class="dropzone" id="my-awesome-dropzone">
                     <div class="fallback">
                         <input name="file" type="file" multiple />
                     </div> 
