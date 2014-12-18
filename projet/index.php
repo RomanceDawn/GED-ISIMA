1aa57455378b71c12dbb949728eb09d7b6bc6fe5
RomanceDawn <enix70@msn.com>
2014-12-18 01:34:41 +0100

    Première ébauche de multi uplaod
    En utilisant "Dropzone.js"

--- projet/index.php
+++ projet/index.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>ISIMA</title>
+        <script src="./javascript/dropzone.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/dropzone.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/base.css">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/etat.css">
     </head>
     <body>
-        <?php
-        echo "coucou";
-        ?>
+        <nav>
+            <ul id="mainMenu">
+                <li><a>Ajouter</a></li>
+                <li><a>Modifier</a></li>
+                <li><a>Supprimer</a></li>
+                <li class="flotteDroite"><a>Déconexion</a></li>
+            </ul>
+        </nav>
+        <div id="corp">
+
+            <section id="centre">
+                <form action="/file-upload" class="dropzone" id="my-awesome-dropzone">
+                    <div class="fallback">
+                        <input name="file" type="file" multiple />
+                    </div> 
+                </form>
+            </section>
+        </div>
     </body>
 </html>
