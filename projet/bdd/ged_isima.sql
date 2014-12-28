244a908c29ecc2a643b5143afb4ad34e8b22174b
RomanceDawn <enix70@msn.com>
2014-12-28 01:46:06 +0100

    Modif. BDD

---
+++ projet/bdd/ged_isima.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Dim 28 Décembre 2014 à 01:43
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `ged_isima`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ged_rapport`
+--
+
+CREATE TABLE IF NOT EXISTS `ged_rapport` (
+  `id` int(10) NOT NULL,
+  `date` date DEFAULT NULL,
+  `mots_clefs` varchar(200) DEFAULT NULL,
+  `nom_origin` varchar(200) DEFAULT NULL,
+  `nom_server` varchar(200) DEFAULT NULL,
+  `auteur` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
