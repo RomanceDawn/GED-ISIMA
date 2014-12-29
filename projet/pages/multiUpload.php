47cdcd1537806e25c4b41ee0c0544107cd3e4362
RomanceDawn <enix70@msn.com>
2014-12-29 19:03:41 +0100

    Controle le type de fichier coté client (pdf/doc/docx

--- projet/pages/multiUpload.php
+++ projet/pages/multiUpload.php
@@ -1,15 +1,28 @@
-<?php include("./header.php");?>
-        <div id="corp">
-            <section id="centre">
-                <form action="../php/multiUploadManager.php" class="dropzone" id="my-awesome-dropzone">
-                    <div class="fallback">
-                        <input name="file" type="file" multiple />
-                    </div> 
-                </form>
-                <?php
+<?php include("./header.php"); ?>
+<div id="corp">
+    <section id="centre">        
+        <script type="text/javascript">
+// "myAwesomeDropzone" is the camelized version of the HTML element's ID
+            Dropzone.options.myAwesomeDropzone = {
+                paramName: "file", // The name that will be used to transfer the file
+                maxFilesize: 50, // MB
+                acceptedFiles: ".pdf,.doc,.docx,.odt,.PDF,.ODT,.DOC,.DOCX",
+                accept: function (file, done) {
+//                    if (file.name == "justinbieber.jpg") {
+//                        done("Naha, you don't.");
+//                    } else {
+                        done();
+//                    }
+                }
+            };
+        </script>
+        <form action="../php/multiUploadManager.php" class="dropzone" id="my-awesome-dropzone">
+            <div class="fallback">
+                <input name="file" type="file" multiple />
+            </div> 
+        </form>
 
 
-                ?>
-            </section>
-        </div>
+    </section>
+</div>
 <?php include("./footer.php"); ?>
