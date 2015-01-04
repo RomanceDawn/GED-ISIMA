351b985d6256422a3b9b490297dd4fc54b71e72c
RomanceDawn <enix70@msn.com>
2015-01-04 17:53:49 +0100

    delete

--- projet/javascript/customDropzone.js
+++ projet/javascript/customDropzone.js
@@ -24,6 +24,31 @@ Dropzone.options.myAwesomeDropzone = {
             file.serverId = response;
             alert(response);
         });
+        this.on("removedfile", function (file) {
+            alert(file.serverId);
+
+//            $.ajax({
+//                url: "../php/delete.php",
+//                type: "POST",
+//                data: {'id': file.serverId}
+//            });   
+            try
+            {
+                var XhrObj = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            catch (e)
+            {
+                var XhrObj = new XMLHttpRequest();
+            }
+
+
+
+            XhrObj.open("POST", "../php/delete.php");
+            XhrObj.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            XhrObj.send("id=" + file.serverId );
+
+
+        });
     },
     paramName: "file", // The name that will be used to transfer the file
     maxFilesize: 64, // MB
@@ -36,7 +61,7 @@ Dropzone.options.myAwesomeDropzone = {
         done();
 //                    }
     },
-    previewTemplate: "<div class=\"table table-striped\" class=\"files\" id=\"previews\"> <div id=\"template\" class=\"file-row\">     <!-- This is used as the file preview template -->      <div>         <p class=\"name\" data-dz-name></p>         <strong class=\"error text-danger\" data-dz-errormessage></strong>     </div>     <div>         <p class=\"size\" data-dz-size></p>         <div class=\"progress progress-striped active\" role=\"progressbar\" aria-valuemin=\"0\" aria-valuemax=\"100\" aria-valuenow=\"0\">           <div class=\"progress-bar progress-bar-success\" style=\"width:0%;\" data-dz-uploadprogress></div>         </div>     </div>     <div>       <button class=\"btn btn-primary start\">           <i class=\"glyphicon glyphicon-upload\"></i>           <span>Start</span>       </button>       <button data-dz-remove class=\"btn btn-warning cancel\">           <i class=\"glyphicon glyphicon-ban-circle\"></i>           <span>Cancel</span>       </button>       <button data-dz-remove class=\"btn btn-danger delete\">         <i class=\"glyphicon glyphicon-trash\"></i>         <span>Delete</span>       </button>     </div>   </div>   </div>"
+    previewTemplate: "<div class=\"table table-striped\" class=\"files\" id=\"previews\"> <div id=\"template\" class=\"file-row\">     <!-- This is used as the file preview template -->      <div>         <p class=\"name\" data-dz-name></p>         <strong class=\"error text-danger\" data-dz-errormessage></strong>     </div>     <div>         <p class=\"size\" data-dz-size></p>         <div class=\"progress progress-striped active\" role=\"progressbar\" aria-valuemin=\"0\" aria-valuemax=\"100\" aria-valuenow=\"0\">           <div class=\"progress-bar progress-bar-success\" style=\"width:0%;\" data-dz-uploadprogress></div>         </div>     </div>     <div>        <button data-dz-remove class=\"btn btn-danger btn-sm delete\">         <i class=\"glyphicon glyphicon-trash\"></i>         <span>Delete</span>       </button>     </div>   </div>   </div>"
 
 };
 
--- projet/php/QueryManager.class.php
+++ projet/php/QueryManager.class.php
@@ -1,26 +1,26 @@
 <?php
+
 //require  './DataAccessObject.class.php';
 include_once '../php/DataAccessObject.class.php';
+
 /**
  * Create queries and execute them using the DAO
  */
 class QueryManager {
 
-      public static function insert(Rapport $object) {
+    public static function insert(Rapport $object) {
         $keys = "(";
         $values = "(";
         foreach ($object->getAttributes() as $col => $value) {
             $keys .= " `" . $col . "`,";
             if ($value != null) {
                 //$value = mysql_real_escape_string($value);
-                
-                $value = mysqli_real_escape_string(DataAccessObject::getInstance()->getLink(),$value);
+
+                $value = mysqli_real_escape_string(DataAccessObject::getInstance()->getLink(), $value);
                 $values .= " '" . $value . "',";
-            }else
-            {
-                 $values .= ' NULL,';
+            } else {
+                $values .= ' NULL,';
             }
-               
         }
         $keys[strlen($keys) - 1] = ")";
         $values[strlen($values) - 1] = ")";
@@ -37,5 +37,30 @@ class QueryManager {
             throw new ErrorException("Erreur avec la base de données.", null, null, null, null, $e1);
         }
     }
+
+    public static function delete($id) {
+
+        $requete = "DELETE FROM `ged_rapport` WHERE `id` = ".$id;
+        try {
+            $DAO = DataAccessObject::getInstance();
+            $DAO->query($requete);
+            return $DAO->getLastInsertedID();
+        } catch (Exception $e1) {
+            throw new ErrorException("Erreur avec la base de données.", null, null, null, null, $e1);
+        }
+    }
     
+        public static function getServer_Name($id) {
+
+        $requete = "SELECT `nom_server`FROM `ged_rapport` WHERE `id` = ".$id;
+        try {
+            $DAO = DataAccessObject::getInstance();
+            $result = $DAO->query($requete);
+            $name = $DAO->fetch($result);
+            return $name[0];
+        } catch (Exception $e1) {
+            throw new ErrorException("Erreur avec la base de données.", null, null, null, null, $e1);
+        }
+    }
+
 }
