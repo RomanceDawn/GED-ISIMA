d3194693af4b758aa24ed420cbd96db87c8ffc00
RomanceDawn <enix70@msn.com>
2015-01-08 17:39:57 +0100

    Nettoyage

--- projet/php/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../php/QueryManager.class.php';
-$id = $_POST['id'];
-$name_server=QueryManager::getServer_Name($id);
-echo $name_server;
-unlink('../rapports/'.$name_server);
-QueryManager::delete(1);
