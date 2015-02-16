dfd170f0621c993a9b2f69f91e6cd06924a56d26
RomanceDawn <enix70@msn.com>
2015-02-16 16:40:05 +0100

    Dates

--- projet/pages/recherche.php
+++ projet/pages/recherche.php
@@ -47,21 +47,12 @@
                     <td> 
                     <select name="annee" id="annee">
                         <option value=""></option>
-                        <option value="2015">2015</option>
-                        <option value="2014">2014</option>
-                        <option value="2013">2013</option>
-                        <option value="2012">2012</option>
-                        <option value="2011">2011</option>
-                        <option value="2010">2010</option>
-                        <option value="2009">2009</option>
-                        <option value="2008">2008</option>
-                        <option value="2007">2007</option>
-                        <option value="2006">2006</option>
-                        <option value="2005">2005</option>
-                        <option value="2004">2004</option>
-                        <option value="2003">2003</option>
-                        <option value="2002">2002</option>
-                        <option value="2001">2008</option>
+                        <?php
+                        $tmp = intval(date('o'));
+                        for ($index = $tmp; $index >=1993 ; $index--) {
+                            echo '<option value="'.$index.'">'.$index.'</option>';    
+                        }
+                        ?>
                     </select>
                     </td>
                 </tr>
