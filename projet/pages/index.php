828498910a21f0aa0f1f9ce82b811281442b8a64
RomanceDawn <enix70@msn.com>
2015-02-23 00:07:24 +0100

    idem

--- projet/pages/index.php
+++ projet/pages/index.php
@@ -6,13 +6,17 @@
 
 <p class="text-center">Ici on va expliquer des trucs.</p> 
 
-    <div class="jumbotron " style="transform: translate(0,50%);">
+    <div class="jumbotron " style="  position: absolute;
+  top: 50%;
+  left:50%;
+  transform: translate(-50%,-50%);
+  width: 80%">
 
         <p class="text-center">Ici on va expliquer des trucs.</p> 
         <form class="form-horizontal" method="post" action="../php/rechercheManager.php" enctype="multipart/form-data">
             <fieldset>
 
-                <div class="col-md-6 col-md-offset-3">
+                <div class="col-md-8 col-md-offset-2">
 
                     <div class="center col-md-12 input-group">
                         <input class="form-control" type="text" name="motsClefs" value="" id="motsClefs" />
